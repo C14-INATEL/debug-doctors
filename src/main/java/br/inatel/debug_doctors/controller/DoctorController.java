310a950112cfcc56e66c8dbb3954bf6b972e7d52
Yan Fernandes <yanfernandesj100@gmail.com>
2026-03-18 18:08:24 -0300

    Cria model uma classe para Medico

--- src/main/java/br/inatel/debug_doctors/controller/DoctorController.java
+++ src/main/java/br/inatel/debug_doctors/controller/DoctorController.java
@@ -1,8 +1,13 @@
 package br.inatel.debug_doctors.controller;
 
+import br.inatel.debug_doctors.model.Medico;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/doctor")
@@ -11,4 +16,17 @@ public class DoctorController {
     public String getDoctor() {
       return "Hello Doctor!";
     }
-}
+
+    @GetMapping
+    public List<Medico> listarMedicos(){
+            // vamos simulando um médico
+        Medico medico1 = new Medico();
+        medico1.setId(1L);
+        medico1.setNome("Dr. Gregory House");
+        medico1.setEspecialidade("Ortopedista");
+        medico1.setCrm("12345-MG");
+        medico1.setInicioExpediente(LocalTime.of(8, 0)); // 08:00
+        medico1.setFimExpediente(LocalTime.of(18, 0));   // 18:00
+
+        return Arrays.asList(medico1);
+    }}
