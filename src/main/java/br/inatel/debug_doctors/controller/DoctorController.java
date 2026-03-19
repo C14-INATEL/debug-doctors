f0636a1ca43e7ff3f02788b4c3b93686eef6dce3
Yan Fernandes <yanfernandesj100@gmail.com>
2026-03-18 21:22:50 -0300

    feat: adicionei a classe doctor

--- src/main/java/br/inatel/debug_doctors/controller/DoctorController.java
+++ src/main/java/br/inatel/debug_doctors/controller/DoctorController.java
@@ -1,6 +1,5 @@
 package br.inatel.debug_doctors.controller;
 
-import br.inatel.debug_doctors.model.Medico;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,18 +14,5 @@ public class DoctorController {
     @GetMapping("/get")
     public String getDoctor() {
       return "Hello Doctor!";
-    }
-
-    @GetMapping
-    public List<Medico> listarMedicos(){
-            // vamos simulando um médico
-        Medico medico1 = new Medico();
-        medico1.setId(1L);
-        medico1.setNome("Dr. Gregory House");
-        medico1.setEspecialidade("Ortopedista");
-        medico1.setCrm("12345-MG");
-        medico1.setInicioExpediente(LocalTime.of(8, 0)); // 08:00
-        medico1.setFimExpediente(LocalTime.of(18, 0));   // 18:00
-
-        return Arrays.asList(medico1);
     }}
+
--- src/main/java/br/inatel/debug_doctors/domain/doctor/Doctor.java
+++ src/main/java/br/inatel/debug_doctors/domain/doctor/Doctor.java
@@ -1,7 +1,26 @@
 package br.inatel.debug_doctors.domain.doctor;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+import java.time.LocalTime;
+
+@Data
+@Entity
 @Table(name = "doctor")
 public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // O banco vai auto-incrementar
+    private Long id;
+
+    private String name;
+    private String specialty;
+    private String crm;
+    private LocalTime shiftStart;
+    private LocalTime shiftEnd;
 }
