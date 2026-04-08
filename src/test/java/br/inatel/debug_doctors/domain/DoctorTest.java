86dbacbde77e392164382e95d4be9ac8e34a96a1
Yan Fernandes <yanfernandesj100@gmail.com>
2026-04-07 23:23:06 -0300

    test: add unit tests for doctor and patient

--- src/test/java/br/inatel/debug_doctors/domain/DoctorTest.java
+++ src/test/java/br/inatel/debug_doctors/domain/DoctorTest.java
@@ -39,8 +39,34 @@ public class DoctorTest {
 
         assertEquals(start, doctor.getShiftStart());
         assertEquals(end, doctor.getShiftEnd());
+    }
 
+    @Test
+    void shouldVerifyIfShiftIsRegistered() {
 
+        Doctor doctor = new Doctor();
+
+        // recording the times
+        doctor.setShiftStart(LocalTime.of(10, 0)); // 10:00
+        doctor.setShiftEnd(LocalTime.of(16, 0));   // 16:00
+
+        // Check if the system recognizes that you are now registered.
+        assertNotNull(doctor.getShiftStart(), "The start time must be recorded.");
+        assertNotNull(doctor.getShiftEnd(), "The end time should be recorded.");
+    }
+
+    @Test
+    void shouldReturnCorrectIdAfterSetting() {
+        // Arrange
+        Doctor doctor = new Doctor();
+        Long simulatedDatabaseId = 105L;
+
+        // Act
+        doctor.setId(simulatedDatabaseId);
+
+        // Assert
+        assertEquals(simulatedDatabaseId, doctor.getId(),
+                "The returned ID should be equal to the defined ID.");
     }
 
 }
