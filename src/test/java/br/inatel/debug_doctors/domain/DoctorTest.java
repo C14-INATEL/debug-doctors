13d8f8ea062c376b163842baf219e313a6ef2fa7
Yan Fernandes <yanfernandesj100@gmail.com>
2026-04-07 23:58:39 -0300

    test: add unit tests for doctor and patient

--- src/test/java/br/inatel/debug_doctors/domain/DoctorTest.java
+++ src/test/java/br/inatel/debug_doctors/domain/DoctorTest.java
@@ -120,4 +120,45 @@ public class DoctorTest {
                 () -> assertEquals(shiftStart, doctor.getShiftStart(), "shiftStart should match the assigned value"),
                 () -> assertEquals(shiftEnd, doctor.getShiftEnd(), "shiftEnd should match the assigned value"));
     }
+
+    @Test
+    void shouldVerifyIfShiftIsRegistered() {
+
+        Doctor doctor = new Doctor();
+
+        // recording the times
+        doctor.setShiftStart(LocalTime.of(10, 0)); // 10:00
+        doctor.setShiftEnd(LocalTime.of(16, 0));   // 16:00
+
+        // Check if the system recognizes that you are now registered.
+        assertNotNull(doctor.getShiftStart(), "The start time must be recorded.");
+        assertNotNull(doctor.getShiftEnd(), "The end time should be recorded.");
+    }
+
+    @Test
+    void shouldReturnCorrectIdAfterSetting() {
+        // Arrange
+        Doctor doctor = new Doctor();
+        Long simulatedDatabaseId = 105L;
+
+        // Act
+        doctor.setId(simulatedDatabaseId);
+
+        // Assert
+        assertEquals(simulatedDatabaseId, doctor.getId(),
+                "The returned ID should be equal to the defined ID.");
+    }
+
+    @Test
+    void shouldSetShiftHoursCorrectly(){
+        Doctor doctor = new Doctor();
+        LocalTime start = LocalTime.of(8,0); // 8:00
+        LocalTime end =  LocalTime.of(18,0);// 18:00
+
+        doctor.setShiftStart(start);
+        doctor.setShiftEnd(end);
+
+        assertEquals(start, doctor.getShiftStart());
+        assertEquals(end, doctor.getShiftEnd());
+    }
 }
